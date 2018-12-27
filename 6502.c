210bdb3fc3efdffb731a6c59ff64ddc11e9e5fae
Sam Norman <samuelenorman@yahoo.co.uk>
2018-12-27 21:15:57 +0000

    Added ncurses for debugging purposes

--- 6502.c
+++ 6502.c
@@ -1,22 +1,29 @@
+#include <ncurses.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-int    zero_flag;
-int    sign_flag;
-int    overflow_flag;
-int    break_flag;
-int    decimal_flag;
-int    interrupt_flag;
-int    carry_flag;
-int    clk;
+int zero_flag;
+int sign_flag;
+int overflow_flag;
+int break_flag;
+int decimal_flag;
+int interrupt_flag;
+int carry_flag;
+int clk;
+
+void outputregisters();
+void outputmemory();
+void updatescreen();
 
 unsigned int program_counter;
 unsigned char stack_pointer,status_register,x_reg,y_reg,accumulator;
 
 unsigned char *memory;
 
-int main() {
+int pages = 0;
 
+int main()
+{	
 	memory = (unsigned char *)malloc(65536);
 
 	memory[0x1000] = 0xA9;
@@ -32,16 +39,32 @@ int main() {
 
 	program_counter = 0x1000;
 
-	int a;
 	unsigned short addr;
 
-   	for( a = 0; a < 3; a = a + 1 ){
-      switch (memory[program_counter++])  {
+	int ch;
+
+	initscr();
+
+	keypad(stdscr, TRUE);
+
+	while(1){
+		ch = getch();
+		if(ch == KEY_LEFT) {
+			break;
+		}
+
+		if(ch == KEY_RIGHT) {
+			pages++;
+			outputmemory(pages);
+		}
+
+		switch (memory[program_counter++])  {
 		case 0xA9:  
 		    accumulator = memory[program_counter];
 		    program_counter++;   
 	        sign_flag = accumulator & 0x80;
 	        zero_flag = !(accumulator);
+	        printw("LDA ");
 	 	break;
 
 		case 0x4c: /* JMP absolute */
@@ -58,21 +81,48 @@ int main() {
  		break;
 	}
 
-	printf("A %x\n", accumulator);
-   }
+		updatescreen();
+	
+	}
 
-
-	return 1;
-
+	endwin();
+	return 0;
 }
 
-void reset_cpu(void)
+void updatescreen()
 {
-	status_register = 0x20;       
-	zero_flag = sign_flag = overflow_flag = break_flag = 0;
-	decimal_flag = interrupt_flag = carry_flag = 0;
-	stack_pointer = 0xFF;
-	program_counter = (memory[0xFFFD] << 8) | memory[0xFFFC];
-	clk=0;
-	accumulator=x_reg=y_reg=0;
+		clear();
+		outputregisters();
+		outputmemory();
+}
+
+void outputregisters(){
+	printw("Accumulator ");
+	printw("0x%x", accumulator);
+	printw("\n");
+	printw("PC ");
+	printw("0x%x", program_counter);
+}
+
+void outputmemory() 
+{
+	mvprintw(0,20,"%s","0x00 0x01 0x02 0x03 0x04 0x05 0x06 0x07 0x08 0x09 0x0A 0x0B 0x0C 0x0D 0x0E 0x0F");
+	mvprintw(1,20,"%s","0x10");
+	mvprintw(2,20,"%s","0x20");
+	mvprintw(3,20,"%s","0x30");
+	mvprintw(4,20,"%s","0x40");
+	mvprintw(5,20,"%s","0x50");
+	mvprintw(6,20,"%s","0x60");
+	mvprintw(7,20,"%s","0x70");
+	mvprintw(8,20,"%s","0x80");
+	mvprintw(9,20,"%s","0x90");
+	mvprintw(10,20,"%s","0xA0");
+	mvprintw(11,20,"%s","0xB0");
+	mvprintw(12,20,"%s","0xC0");
+	mvprintw(13,20,"%s","0xD0");
+	mvprintw(14,20,"%s","0xE0");
+	mvprintw(15,20,"%s","0xF0");
+
+	mvprintw(17,20,"Page %x", pages);
+
 }
